6dd131d1ebcf02dc8dcd6400bb07289f1443d860
Denis Stanovoy <stanovoyd@yandex.ru>
2022-12-01 11:45:52 +0300

    fixed bugs with the removal of the task when the app interacts with the databas

--- src/controller/deleteTodo.js
+++ src/controller/deleteTodo.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+const objectId = require('mongodb').ObjectId;
 const Todo = require('../db/models/schemeTodo');
 const config = require('../config');
 
 const deleteTodo = async (req, res) => {
   try {
     await mongoose.connect(config.mongoUrl);
-    const _id = Todo._id;
-    const result = await Todo.deleteOne(_id);
-    res.json(result);
+    const _id = objectId(req.params.todoId);
+    await Todo.deleteOne(_id);
+    console.log(_id);
+    res.sendStatus(204);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
